a3f97dd3125670808bc827134ed8cd176adccc99
ashu1-12 <sadanshivashish122@gmail.com>
2023-08-22 17:53:01 +0530

    Resolved Conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class Main{
 	public static void main(String[] args){
 		System.out.println("Welcome to the World of Linux Commands");
-		System.out.println("Wrote this line in remote");//changes made in gitHub
-		//Ashish Sadanshiv
 	}
 }
